b0790090d01a0e1e823b67a9dcd0984c57e41bef
Christopher Davis (PhD Dept ElecElecComp Eng FT) <cxd309@student.bham.ac.uk>
2024-11-26 12:44:16 +0000

    Initial Commit
    Copied the existing files over from where they were being worked on before.
    
    Modified README.md to copy information from readme.txt but did some markdown formatting.
    
    Added a .gitignore to resampled folder as they shouldn't need to be recreated

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# bcrre_rcm_lab_data_resample
- Resampling of data from BCRRE remote condition monitoring lab
+# BCRRE Remote Condition Monitoring Lab - Data Resampling
+*Author: Chris Davis, email: cxd309@student.bham.ac.uk*
+
+## Introduction
+This is a resample of the data taken during the MSc Remote Condition Monitoring Labs.
+The aim being to make it easier for some student to work with the datasets on their
+computers.
+
+### Information for Students
+If you use these in your final reports you should detail exactly which dataset is being
+used. The method used to resample the data will impact the data itself and you should
+write how it will be affected in your report. The detail on method used is all explained
+below and if you need more information you can always ask.
+
+## Method
+The python file 'resample.py' was created and uses the pandas package to interact 
+with the .csv input files.
+
+There are two methods for downsampling
+* simple
+* average
+
+Both use downsample with a ratio n from the captured data frequency of 2kHz.
+For example, to downsample to 100Hz, a ratio n=20 is used.
+
+The chosen frequencies to resample at are:
+* 1000Hz (n=2)
+* 500Hz  (n=4)
+* 250Hz  (n=8)
+* 200Hz  (n=10)
+* 100Hz  (n=20)
+
+### simple downsampling
+
+To downsample the programme takes a ratio n 
+it then selects every nth line from the original file.
+For instance to downsample from 2000Hz to 1000Hz
+the ratio is 2 (n=2) so the programme selects rows 1, 3, 5, 7, etc.
+
+### average downsampling
+To downsample the programme takes a ratio n 
+it then groups every n lines from the original file
+and takes an average of those.
+for instance to downsample from 2000Hz to 1000Hz
+the ratio is 2 (n=2) so the programme groups and averages
+rows 1&2, 3&4, 5&6, 7&8, etc.
+
---
+++ resample.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+def Resample_File(name):
+    fileName = "raw_data\\"+name+".csv"
+    og_df = pd.read_csv(fileName)
+    ratio_list = [2,4,8,10,20]
+    original_frequency = 2000
+    for ratio in ratio_list:
+        Downsample_DF(og_df, ratio, name, original_frequency)
+        
+def Conv_StringTime_Seconds(string_time):
+    h, m, s = map(int, string_time.split(':'))
+    return h * 3600 + m * 60 + s
+
+def Conv_Seconds_StringTime(seconds):
+    h = int(seconds // 3600)
+    m = int((seconds % 3600) // 60)
+    s = int(seconds % 60)
+    return f"{h:02}:{m:02}:{s:02}"
+
+def Simple_Downsample_DF(og_df, ratio):
+    return og_df.iloc[::ratio]
+
+def Average_Downsample_DF(og_df, ratio):
+    avg_df = og_df.copy(deep=True)
+    avg_df['Time [HH:mm:ss]'] = avg_df['Time [HH:mm:ss]'].apply(Conv_StringTime_Seconds)
+    #avg_df = og_df.apply(pd.to_numeric, errors='coerce')
+    avg_df = avg_df.groupby(avg_df.index // ratio).mean()
+    avg_df['Time [HH:mm:ss]'] = avg_df['Time [HH:mm:ss]'].apply(Conv_Seconds_StringTime)
+    
+    return avg_df
+    
+def Downsample_DF(og_df, ratio, name, original_frequency):
+    new_frequency = original_frequency/ratio
+    
+    simple_downsample_df = Simple_Downsample_DF(og_df, ratio)
+    avg_downsample_df = Average_Downsample_DF(og_df, ratio)
+    
+    Save_DF(simple_downsample_df, name, "simple", new_frequency)
+    Save_DF(avg_downsample_df, name, "average", new_frequency)
+    
+def Save_DF(df, name, ds_method, freq):
+    file_name = "resampled\\" + name + " - " + ds_method + " downsample " + str(int(freq)) + "Hz.csv"
+    print("Saving: "+file_name)
+    df.to_csv(file_name, encoding='utf-8', index=False)
+    
+def Main():
+    print("starting resampling")
+    Resample_File("day_1_data")
+    Resample_File("day_2_group-1-4_data")
+    Resample_File("day_2_group-5-8_data")
+    print("complete")
+    
+Main()
