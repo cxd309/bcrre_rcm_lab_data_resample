e3f622ae0e261fd968600b24808ba01476506819
Chris Davis <cxd309@student.bham.ac.uk>
2024-11-28 15:10:41 +0000

    Added file picker
    Added simple cli to select a file stored in raw_data and then create the plot

--- plot.py
+++ plot.py
@@ -2,8 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-def Plot_Figure(file_path, n):
-    fig = plt.figure(n)
+def Plot_Figure(file_path):
     raw_df = pd.read_csv(file_path)
     
     # correct for gravity in z axis
@@ -17,16 +16,32 @@ def Plot_Figure(file_path, n):
     plt.ylabel("Acceleration (m/s/s)")
     plt.title(os.path.basename(file_path))
     plt.legend()
-    fig.show()
-
-def Plot_Raw_Data():
+    plt.show()
+    
+def Get_Raw_Data_Contents():
     raw_data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"raw_data")        
-    raw_file_list = [os.path.join(raw_data_dir, f) for f in os.listdir(raw_data_dir) if os.path.isfile(os.path.join(raw_data_dir, f))]  
-    
-    for i in range(len(raw_file_list)):
-        file_path = raw_file_list[i]
-        Plot_Figure(file_path, i+1)
-    
-Plot_Raw_Data()
+    return [os.path.join(raw_data_dir, f) for f in os.listdir(raw_data_dir) if os.path.isfile(os.path.join(raw_data_dir, f)) and os.path.splitext(f)[1]== ".csv"]
 
-input()
+def File_Selector():
+    file_list = Get_Raw_Data_Contents()
+    
+    if len(file_list)<1:
+        print("No valid files in directory")
+        input()
+        return
+    
+    valid_option_list = []
+    print("Select file to plot:")
+    for i in range(len(file_list)):
+        file_path = file_list[i]
+        print(str(i)+". "+os.path.basename(file_path))
+        valid_option_list.append(str(i))
+    file_no = input()
+    if file_no in valid_option_list:
+        Plot_Figure(file_list[int(file_no)])
+    else:
+        print("Invalid option selection")
+        input()
+    
+while(1):
+    File_Selector()
